d688c3f3b136c80d948f0f8bac60d5364292acb6
felipeluizmsouza@gmail.com <felipeluizmsouza@gmail.com>
2024-05-29 15:36:18 -0300

    Atividades do While

---
+++ While/NumerosNegativos.cs
@@ -0,0 +1,17 @@
+﻿namespace WhileDoWhile.While;
+
+public class NumerosNegativos : IQuestao
+{
+    public NumerosNegativos()
+    {
+        Console.WriteLine("escreva 10 valores separados por espaços");
+        Console.WriteLine(
+            Console.ReadLine()!
+                .Split(' ')
+                .Select(e => Convert.ToInt32(e.Trim()))
+                .Where(e => e < 0)
+                .ToList()
+                .Count
+        );
+    }
+}
