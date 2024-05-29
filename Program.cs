d45b88d8ac79b4862b1ff0f66798152da7fea766
felipeluizmsouza@gmail.com <felipeluizmsouza@gmail.com>
2024-05-29 15:37:04 -0300

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace WhileDoWhile;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool continuar = true;
+        while (continuar)
+        {
+            new Menu()
+                .GetQuestao();
+            Console.WriteLine("quer continuar? (sim ou não)");
+            continuar = Console.ReadLine() == "sim";
+            Console.Clear();
+        }
+    }
+}
