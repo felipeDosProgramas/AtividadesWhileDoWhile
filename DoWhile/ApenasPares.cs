9787f76109fa554531d3a80825bba511b8831128
felipeluizmsouza@gmail.com <felipeluizmsouza@gmail.com>
2024-05-29 15:35:36 -0300

    Atividades do Do DoWhile

---
+++ DoWhile/ApenasPares.cs
@@ -0,0 +1,20 @@
+﻿namespace WhileDoWhile.DoWhile;
+
+public class ApenasPares : IQuestao
+{
+    public ApenasPares()
+    {
+        int num;
+
+        do
+            GetNumero();
+        while ((num = Convert.ToInt32(Console.ReadLine())) % 2 != 0);
+        Console.WriteLine(num);
+    }
+
+    private void GetNumero()
+    {
+        Console.Clear();
+        Console.WriteLine("escreva um número par");
+    }
+}
